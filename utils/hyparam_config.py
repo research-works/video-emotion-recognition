680ce39cc5e11d83373725f2bd11c1ff4be7aafb
reckoner1429 <abhishekbisht1429@gmail.com>
2020-07-15 00:45:44 +0530

    added value 'iteration' in hyperparam

--- utils/hyparam_config.py
+++ utils/hyparam_config.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 HyperparamConfig2 = {
+    'iteration': 2,
     'layer1_kernel_intializer': 'glorot_uniform',
     'layer1_kernel_regularizer': tf.keras.regularizers.l1(0.01),
     'layer1_activity_regulazier':  tf.keras.regularizers.l2(0.01),
@@ -9,6 +10,7 @@ HyperparamConfig2 = {
 }
 
 HyperparamConfig3 = {
+    'iteration': 3,
     'layer1_kernel_intializer': 'glorot_uniform',
     'layer1_kernel_regularizer': None,
     'layer1_activity_regulazier':  None,
