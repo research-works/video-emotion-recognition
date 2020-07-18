22657e3d5fb2377e8968b9b4c2b1a5d70557435d
dikshamehta <500061597@stu.upes.ac.in>
2020-07-19 01:39:28 +0530

    new hyperparam

--- utils/hyparam_config.py
+++ utils/hyparam_config.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-# Naming convection 
+# Naming convection
 # A - Abhishek
 HyperparamConfigA2 = {
     'iteration': 'A2',
@@ -10,7 +10,7 @@ HyperparamConfigA2 = {
     'layer1_dropout': tf.keras.layers.Dropout(0.2),
     'optimizer_ft': 'rmsprop',
     'optimizer_fusion': tf.keras.optimizers.RMSprop(),
-    'batch_size': 8, 
+    'batch_size': 8,
     'epochs': 200
 }
 
@@ -51,4 +51,17 @@ HyperparamConfigTest = {
     'optimizer_fusion': tf.keras.optimizers.RMSprop(learning_rate = 0.0001),
     'batch_size': 8,
     'epochs': 1
+}
+
+HyperparamConfigD1 = {
+    'iteration': 'D1',
+    'layer1_kernel_intializer': 'glorot_uniform',
+    'layer1_kernel_regularizer': None,
+    'layer1_activity_regulazier':  None,
+    'layer1_activation': 'relu',
+    'layer1_dropout': tf.keras.layers.Dropout(0.2),
+    'optimizer_ft': 'rmsprop',
+    'optimizer_fusion': tf.keras.optimizers.RMSprop(learning_rate = 0.0001),
+    'batch_size': 8,
+    'epochs': 200
 }
