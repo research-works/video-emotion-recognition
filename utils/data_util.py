c2dcf14832e119d15c62213557584b99d98588cf
reckoner1429 <abhishekbisht1429@gmail.com>
2020-07-23 17:32:03 +0530

    made changes in order to train models on SAVEE

--- utils/data_util.py
+++ utils/data_util.py
@@ -6,84 +6,17 @@ import utils.audio_utils as audio_utils
 import utils.video_utils as video_utils
 import tensorflow.keras as keras
 import utils.local_config as local_config
+from utils.preprocess_util import load_audio_filenames
+from utils.preprocess_util import load_facial_filenames
 
 DATA_DIR = local_config.DATA_DIR
 PREPROCESSED_VIDEO_DIR = local_config.PREPROCESSED_VIDEO_DIR
 PREPROCESSED_AUDIO_DIR = local_config.PREPROCESSED_AUDIO_DIR
-EMOTION_CLASSES = ['neutral', 'calm', 'happy', 'sad','angry','fearful','disgust','surprised']
+# EMOTION_CLASSES = ['neutral', 'calm', 'happy', 'sad','angry','fearful','disgust','surprised']
 
 SAMPLE_RATE = local_config.SAMPLE_RATE
 DURATION = local_config.DURATION
 OFFSET = local_config.OFFSET
-
-preprocess_audio_data = local_config.preprocess_audio_data
-preprocess_facial_data = local_config.preprocess_facial_data
-# OUTPUT_IMAGE_WIDTH = 299
-# OUTPUT_IMAGE_HEIGHT = 299
-# 01 = neutral, 02 = calm, 03 = happy, 04 = sad, 05 = angry, 06 = fearful, 07 = disgust, 08 = surprised
-# file1 - calm - [0,1,0,0,0,0,0,0]
-# fil2 - angry - [0,0,0,0,1,0,0,0]
-def one_hot(i, n=8):
-    arr = np.zeros(8)
-    arr[i] = 1
-    return arr
-
-def zero_pad(x, n):
-    diff = n - x.shape[0]
-    before = diff // 2
-    after = diff - before
-    x = np.pad(x, (before, after))
-    return x
-
-def load_facial_filenames():
-    X, Y = [], []
-    base_path = PREPROCESSED_VIDEO_DIR
-    print(base_path)
-    for actor_folder in os.listdir(base_path):
-        actor_path = base_path + '/' + actor_folder + '/' + 'subtracted_frames'
-        files_list = []
-        for image_file in os.listdir(actor_path):
-            files_list.append(image_file)
-        files_list.sort()
-        for image_file in files_list:
-            image_path = actor_path + '/' + image_file
-            X.append(image_path)
-
-            em_id = int(image_file.split('-')[2])
-            one_hot_em = one_hot(em_id - 1)
-            # print(one_hot_em)
-            Y.append(one_hot_em)
-    X = np.array(X)
-    Y = np.array(Y)
-    return X, Y
-
-
-def load_audio_filenames():
-    print("hello") # Written by Diksha
-    X, Y = [], []
-    base_path = PREPROCESSED_AUDIO_DIR
-    print(base_path)
-    for actor_folder in os.listdir(base_path):
-        actor_path = base_path + '/' + actor_folder
-        files_list = []
-        for audio_file in os.listdir(actor_path):
-            files_list.append(audio_file)
-        files_list.sort()
-        for audio_file in files_list:
-            audio_path = actor_path + '/' + audio_file
-            # S_input = np.load(audio_path)
-            # # print(S_input.shape)
-            # X.append(S_input) # (216,1)
-            X.append(audio_path)
-
-            em_id = int(audio_file.split('-')[2])
-            one_hot_em = one_hot(em_id - 1)
-            # print(one_hot_em.shape)
-            Y.append(one_hot_em)
-    X = np.array(X)
-    Y = np.array(Y)
-    return X, Y
-
 
 class FaceDataGenerator(keras.utils.Sequence):
 
