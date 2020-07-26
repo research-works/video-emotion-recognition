3226b3133f6e462d4760fed600710e109e704fe2
reckoner1429 <abhishekbisht1429@gmail.com>
2020-07-26 11:45:44 +0530

    added hyparam config a5 and a6

--- utils/hyparam_config.py
+++ utils/hyparam_config.py
@@ -53,6 +53,32 @@ HyperparamConfigA5 = {
     'epochs': 200
 }
 
+HyperparamConfigA6 = {
+    'iteration': 'A5',
+    'layer1_kernel_intializer': 'glorot_uniform',
+    'layer1_kernel_regularizer': None,
+    'layer1_activity_regulazier':  tf.keras.regularizers.l2(0.01),
+    'layer1_activation': 'relu',
+    'layer1_dropout': tf.keras.layers.Dropout(0.2),
+    'optimizer_ft': 'rmsprop',
+    'optimizer_fusion': tf.keras.optimizers.RMSprop(learning_rate = 0.0001),
+    'batch_size': 8,
+    'epochs': 200
+}
+
+HyperparamConfigA6 = {
+    'iteration': 'A6',
+    'layer1_kernel_intializer': 'glorot_uniform',
+    'layer1_kernel_regularizer': None,
+    'layer1_activity_regulazier':  tf.keras.regularizers.l2(0.05),
+    'layer1_activation': 'relu',
+    'layer1_dropout': tf.keras.layers.Dropout(0),
+    'optimizer_ft': 'rmsprop',
+    'optimizer_fusion': tf.keras.optimizers.RMSprop(learning_rate = 0.0001),
+    'batch_size': 8,
+    'epochs': 200
+}
+
 
 
 
