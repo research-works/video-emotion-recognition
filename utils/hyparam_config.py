a4785e5c5c9f24a62e4724a26d72d117d78402c5
reckoner1429 <abhishekbisht1429@gmail.com>
2020-08-12 07:44:30 +0530

    added config A9

--- utils/hyparam_config.py
+++ utils/hyparam_config.py
@@ -81,9 +81,22 @@ HyperparamConfigA7 = {
 
 
 HyperparamConfigA8 = {
-    'iteration': 'A7',
+    'iteration': 'A8',
     'layer1_kernel_intializer': 'glorot_uniform',
     'layer1_kernel_regularizer': None,
+    'layer1_activity_regulazier':  tf.keras.regularizers.l2(0.2),
+    'layer1_activation': 'relu',
+    'layer1_dropout': tf.keras.layers.Dropout(0.3),
+    'optimizer_ft': 'rmsprop',
+    'optimizer_fusion': tf.keras.optimizers.RMSprop(learning_rate = 0.00001),
+    'batch_size': 8,
+    'epochs': 200
+}
+
+HyperparamConfigA9 = {
+    'iteration': 'A9',
+    'layer1_kernel_intializer': 'glorot_uniform',
+    'layer1_kernel_regularizer': tf.keras.regularizers.l2(0.1),
     'layer1_activity_regulazier':  tf.keras.regularizers.l2(0.2),
     'layer1_activation': 'relu',
     'layer1_dropout': tf.keras.layers.Dropout(0.3),
